bdba202f159c0e189896f6a8dd662cf9bf7b0401
James Tease <james.tease@rotork.com>
2016-06-06 15:38:19 +0100

    Now reading from files

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use rustc_serialize::json;
 use rustc_serialize::json::Json;
 
 const MESSAGE_PATTERN: &'static str = "_translations/**/*.json";
-const LANG_DIR: &'static str = "_translations/lang/";
+// const LANG_DIR: &'static str = "_translations/lang/";
 
 #[derive(Debug, RustcDecodable, RustcEncodable)]
 struct Message {
@@ -24,27 +24,24 @@ struct Message {
 //   messages: Vec<Message>
 // }
 
+fn read_file(filename: &Path) {
+  let mut contents = String::new();
+  let mut file = match File::open(filename) {
+    Err(e) => panic!("Couldn't open {}: {}", filename.display(), e.description()),
+    Ok(file) => file
+  };
 
+  let msgs = match file.read_to_string(&mut contents) {
+    Err(e) => panic!("Couldn't read {}: {}", filename.display(), e.description()),
+    Ok(_) => Json::from_str(&contents).unwrap()
+  };
 
-// fn read_file(filename: &Path) {
-//   let mut contents = String::new();
-//   let mut file = match File::open(filename) {
-//     Err(e) => panic!("Couldn't open {}: {}", filename.display(), e.description()),
-//     Ok(file) => file
-//   };
-
-//   match file.read_to_string(&mut contents) {
-//     Err(e) => panic!("Couldn't read {}: {}", filename.display(), e.description()),
-//     Ok(_) => {
-//       let messages: Messages = match json::decode(&contents) {
-//         Err(e) => panic!("Error decoding {}", e.description()),
-//         Ok(messages) => messages
-//       };
-//       println!("{:?}", messages);
-//     }
-//   }
-
-// }
+  let data = msgs.as_array().unwrap();
+  for msg in data {
+    let message: Message = json::decode(&msg.to_string()).unwrap();
+    println!("{:?}", message);
+  }
+}
 
 fn main() {
   // open src files
@@ -57,32 +54,32 @@ fn main() {
   //   \"defaultMessage\": \"Alarms\"
   // }";
 
-  let arr = "[{
-    \"id\": \"alarms.heading\",
-    \"description\": \"Alarm heading text\",
-    \"defaultMessage\": \"Alarms\"
-  },{
-    \"id\": \"alarms.heading2\",
-    \"description\": \"Alarm heading text\",
-    \"defaultMessage\": \"Alarms 2\"
-  }]";
+  // let arr = "[{
+  //   \"id\": \"alarms.heading\",
+  //   \"description\": \"Alarm heading text\",
+  //   \"defaultMessage\": \"Alarms\"
+  // },{
+  //   \"id\": \"alarms.heading2\",
+  //   \"description\": \"Alarm heading text\",
+  //   \"defaultMessage\": \"Alarms 2\"
+  // }]";
 
   // let message: Message = json::decode(raw).unwrap();
   // println!("{:?}", message);
 
-  let msgs = Json::from_str(arr).unwrap();
-  println!("{:?}", msgs);
+  // let msgs = Json::from_str(arr).unwrap();
+  // println!("{:?}", msgs);
 
-  let data = msgs.as_array().unwrap();
-  println!("{:?}", data);
+  // let data = msgs.as_array().unwrap();
+  // println!("{:?}", data);
 
-  for msg in data {
-    let message = msg.as_object().unwrap();
-    println!("{:?}", message);
+  // for msg in data {
+  //   let message = msg.as_object().unwrap();
+  //   println!("{:?}", message);
 
-    let decoded: Message = json::decode(&msg.to_string()).unwrap();
-    println!("{:?}", decoded);
-  }
+  //   let decoded: Message = json::decode(&msg.to_string()).unwrap();
+  //   println!("{:?}", decoded);
+  // }
 
   // for(key, value) in data.iter() {
   //   println!("{:?}: {}", key, match *value {
@@ -91,13 +88,13 @@ fn main() {
   //   });
   // }
 
-  // for entry in glob(MESSAGE_PATTERN).unwrap() {
-  //   match entry {
-  //     Ok(path) => { // path = PathBuf
-  //       println!("{:?}", path);
-  //       read_file(path.as_path());
-  //     },
-  //     Err(e) => println!("{:?}", e)
-  //   }
-  // }
+  for entry in glob(MESSAGE_PATTERN).unwrap() {
+    match entry {
+      Ok(path) => { // path = PathBuf
+        println!("{:?}", path);
+        read_file(path.as_path());
+      },
+      Err(e) => println!("{:?}", e)
+    }
+  }
 }
