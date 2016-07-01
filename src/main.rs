131e16f37b9c68208bad63d6fdfd887175f35469
James Tease <james.tease@rotork.com>
2016-07-01 09:24:42 +0100

    Reading translated files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 extern crate glob;
 extern crate rustc_serialize;
 
+// TODO use box?
+
 use glob::glob;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read; // need this for .read_to_string to exist
 use std::path::Path;
+use std::collections::BTreeMap;
+use std::collections::HashMap;
 use rustc_serialize::json;
 use rustc_serialize::json::Json;
 
 const MESSAGE_PATTERN: &'static str = "_translations/src/**/*.json";
+const TRANSLATION_PATTERN: &'static str = "_translations/lang/**/*.json";
+const en_string: &'static str = "en";
 // const LANG_DIR: &'static str = "_translations/lang/";
 
 #[derive(Debug, RustcDecodable, RustcEncodable)]
@@ -19,10 +25,15 @@ struct Message {
   defaultMessage: String
 }
 
-// #[derive(Debug, RustcDecodable, RustcEncodable)]
-// struct Messages {
-//   messages: Vec<Message>
-// }
+struct TranslationFile {
+  keys: BTreeMap<String, Json>
+}
+
+struct Translation<'a> {
+  key: &'a String,
+  value: String,
+  lang: String
+}
 
 fn read_file(filename: &Path, messages: &mut Vec<Message>) {
   let mut contents = String::new();
@@ -39,58 +50,90 @@ fn read_file(filename: &Path, messages: &mut Vec<Message>) {
   let data = msgs.as_array().unwrap();
   for msg in data {
     let message: Message = json::decode(&msg.to_string()).unwrap();
-    // println!("{:?}", message);
     messages.push(message);
+  }
+
+}
+
+fn read_translation_file(filename: &Path, messages: &Vec<Message>) {
+  // create hashmap
+  let mut translations = HashMap::new();
+  // for each message
+  for message in messages {
+    // println!("{:?}, {:?}", message.id, message.defaultMessage);
+    let mut translation: HashMap<&str, &String> = HashMap::new();
+    translation.insert(en_string, &message.defaultMessage.to_string());
+    translations.insert(&message.id, translation);
+  }
+
+  println!("{:?}", translations);
+  // use id as key
+
+  // get name of file to use as key
+  let lang_file = filename.file_name().unwrap().to_str().unwrap();
+  let lang_name: Vec<&str> = lang_file.split('.').collect();
+  let lang = lang_name[0];
+
+  println!("{:?}", lang);
+
+  let mut contents = String::new();
+  let mut file = match File::open(filename) {
+    Err(e) => panic!("Couldn't open {}: {}", filename.display(), e.description()),
+    Ok(file) => file
+  };
+
+  let msgs = match file.read_to_string(&mut contents) {
+    Err(e) => panic!("Couldn't read {}: {}", filename.display(), e.description()),
+    Ok(_) => Json::from_str(&contents).unwrap()
+  };
+
+  println!("msgs: {:?}", msgs);
+
+  let data = msgs.as_object().unwrap();
+
+  for (key, value) in data.iter() {
+    // println!("{}: {}", key, value);
+    // println!("{}: {}", key, match *value {
+    //   Json::String(ref v) => {
+    //     format!("{} (string)", v)
+    //   },
+    //   Json::Object(ref o) => {
+    //     // translations.push( json::decode(&o.to_string()).unwrap() );
+    //     format!("{:?} object", o) // o is the object (value) where k = 'es'
+    //   },
+    //   _ => format!("other")
+    // });
+
+    let translation = Translation{
+      key: key,
+      value: value.as_string().unwrap().to_string(),
+      lang: lang.to_string()
+    };
+
+    let mut translation_map: HashMap<&str, &String> = HashMap::new();
+    translation_map.insert(&translation.key, &translation.value);
+
+    // println!("key: {:?}", key);
+    // println!("map: {:?}", translations.get(key));
+    // println!("{:?}", translation);
+    // TODO make sure key exists
+    match translations.get(key) {
+      Some(map) => map,
+      None => {
+        translations.insert(&translation.key.to_string(), translation_map);
+        return;
+      }
+    };
+    // en_key.insert(lang, &value.to_string());
+    // translations.insert(key, translation);
   }
 }
 
 fn main() {
-  // open src files
-  // read in eng translation
-  // convert to struct
   // http://zsiciarz.github.io/24daysofrust/book/day6.html
-  // let raw = "{
-  //   \"id\": \"alarms.heading\",
-  //   \"description\": \"Alarm heading text\",
-  //   \"defaultMessage\": \"Alarms\"
-  // }";
 
-  // let arr = "[{
-  //   \"id\": \"alarms.heading\",
-  //   \"description\": \"Alarm heading text\",
-  //   \"defaultMessage\": \"Alarms\"
-  // },{
-  //   \"id\": \"alarms.heading2\",
-  //   \"description\": \"Alarm heading text\",
-  //   \"defaultMessage\": \"Alarms 2\"
-  // }]";
-
-  // let message: Message = json::decode(raw).unwrap();
-  // println!("{:?}", message);
-
-  // let msgs = Json::from_str(arr).unwrap();
-  // println!("{:?}", msgs);
-
-  // let data = msgs.as_array().unwrap();
-  // println!("{:?}", data);
-
-  // for msg in data {
-  //   let message = msg.as_object().unwrap();
-  //   println!("{:?}", message);
-
-  //   let decoded: Message = json::decode(&msg.to_string()).unwrap();
-  //   println!("{:?}", decoded);
-  // }
-
-  // for(key, value) in data.iter() {
-  //   println!("{:?}: {}", key, match *value {
-  //     Json::String(ref v) => format!("{} (string)", v),
-  //     _ => format!("other")
-  //   });
-  // }
-
-  let mut messages = vec![];
-
+  // get eng messages
+  let mut messages: Vec<Message> = vec![];
   for entry in glob(MESSAGE_PATTERN).unwrap() {
     match entry {
       Ok(path) => { // path = PathBuf
@@ -100,6 +143,19 @@ fn main() {
       Err(e) => println!("{:?}", e)
     }
   }
-
   println!("{:?}", messages);
+
+  // get translations
+  // let translations: Vec<BTreeMap<String, Json>> = vec![];
+  for entry in glob(TRANSLATION_PATTERN).unwrap() {
+    match entry {
+      Ok(path) => {
+        read_translation_file(path.as_path(), &messages);
+      },
+      Err(e) => println!("{:?}", e)
+    }
+  }
+  // println!("{:?}", translations);
+
+  // merge by keys
 }
