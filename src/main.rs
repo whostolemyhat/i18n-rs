bfc12860615f5fe67919868e4c8909ad873795d4
James Tease <james.tease@rotork.com>
2016-07-01 16:15:43 +0100

    Replace rust unicode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate glob;
 extern crate rustc_serialize;
+extern crate regex;
 
 use glob::glob;
 use std::error::Error;
@@ -7,8 +8,10 @@ use std::fs::File;
 use std::io::Read; // need this for .read_to_string to exist
 use std::path::Path;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 use rustc_serialize::json;
 use rustc_serialize::json::Json;
+use regex::Regex;
 
 const MESSAGE_PATTERN: &'static str = "_translations/src/**/*.json";
 const TRANSLATION_PATTERN: &'static str = "_translations/lang/**/*.json";
@@ -41,7 +44,7 @@ fn read_file(filename: &Path, messages: &mut Vec<Message>) {
 
 }
 
-fn read_translation_file(filename: &Path, translations: &mut HashMap<String, HashMap<String, String>>) {
+fn read_translation_file(filename: &Path, translations: &mut BTreeMap<String, BTreeMap<String, String>>) {
   // get name of file to use as key
   let lang_file = filename.file_name().unwrap().to_str().unwrap();
   let lang_name: Vec<&str> = lang_file.split('.').collect();
@@ -63,10 +66,9 @@ fn read_translation_file(filename: &Path, translations: &mut HashMap<String, Has
   // to_string everywhere: https://users.rust-lang.org/t/variable-does-not-live-long-enough/936/2
   // converts to String which are on the heap so live long enough
   for translation in data {
-    let translation_map: HashMap<String, String> = HashMap::new();
-    // translation_map.insert(lang.to_string(), translation.1.as_string().unwrap().to_string());
+    let translation_map: BTreeMap<String, String> = BTreeMap::new();
 
-    // not the most efficient way
+    // update existing key or add an empty hashmap under that key
     // http://stackoverflow.com/questions/28512394/how-to-lookup-from-and-insert-into-a-hashmap-efficiently
     // http://doc.rust-lang.org/std/collections/hash_map/enum.Entry.html
     let existing = translations.entry(translation.0.to_string()).or_insert(translation_map);
@@ -88,11 +90,11 @@ fn main() {
     }
   }
 
-  let mut translations = HashMap::new();
+  let mut translations = BTreeMap::new();
 
   // add en to map
   for message in messages {
-    let mut translation: HashMap<String, String> = HashMap::new();
+    let mut translation: BTreeMap<String, String> = BTreeMap::new();
     translation.insert(EN_STRING.to_string(), message.defaultMessage.to_string());
     translations.insert(message.id.to_string(), translation);
   }
@@ -108,7 +110,12 @@ fn main() {
   }
 
   println!("{:?}", translations);
+  // let encoded = json::encode(&translations).unwrap();
+  let encoded = format!("{:?}", translations);
 
-  // convert to JSON
-  // json::encode(translations);
+  let re = Regex::new(r"(\\u\{)([\w]+)(\})+").unwrap();
+  let result = re.replace_all(&encoded, "\\u00$2");
+  println!("{:?}", result);
+  // replace \u{e0} with \u00e0
+  // /(\\u\{[\w]+\})+/g
 }
